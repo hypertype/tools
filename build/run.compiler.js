ddbac493c13e74c1fc30079d02d3de3379c0913f
Andrey <andrey-ne@mail.ru>
2019-04-10 16:51:04 +0300

    fix exit on run

--- build/run.compiler.js
+++ build/run.compiler.js
@@ -1,5 +1,5 @@
 module.exports = function runCompiler(compiler) {
-    if (process.argv.filter(t => /watch/.test(t))){
+    if (process.argv.filter(t => /watch/.test(t)).length) {
         compiler.watch({}, (err, stats) => {
             const info = stats.toJson("minimal");
             if (info.errors.length) {
@@ -12,7 +12,7 @@ module.exports = function runCompiler(compiler) {
             console.log(stats.toString())
             // Done processing
         })
-    }else{
+    } else {
         compiler.run((err, stats) => {
             if (err || stats.hasErrors()) {
                 // Handle errors here
